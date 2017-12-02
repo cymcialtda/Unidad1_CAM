3d3bb5c546049a419fdf14d64ca95c4f950f554e
cesar.montoya <cesar.montoya@DESARROLLO-CAM.LINEADATASCAN.local>
2017-12-02 11:56:39 -0500

    Cambio el Hola Mundo

--- Unidad2-prueba2/src/main/java/com/lds/Unidad2_prueba2/App.java
+++ Unidad2-prueba2/src/main/java/com/lds/Unidad2_prueba2/App.java
@@ -6,8 +6,8 @@ package com.lds.Unidad2_prueba2;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hola World!");
     }
 }
